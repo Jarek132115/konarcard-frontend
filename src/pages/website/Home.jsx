3c5802a68dd656084d5f1e161bdbea661da2de45
Jarek <103086uk@saeinstitute.edu>
2025-07-12 22:17:34 +0100

    Automated update

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -473,7 +473,7 @@ export default function Home() {
               </div>
               {/* Conditional rendering for subscribe button based on subscription status */}
               {loadingStatus ? (
-                <button className="desktop-button combined-section-button" disabled>
+                <button className="desktop-button blue-button" disabled>
                   Loading Plan Status...
                 </button>
               ) : isSubscribed ? (
@@ -514,7 +514,7 @@ export default function Home() {
                 <p className='desktop-h5'>£24.95</p>
                 <p className='light-black' style={{ fontSize: 14 }}>Lifetime Use</p>
               </div>
-              <Link to="/shopnfccards/whitecard" className="desktop-button combined-section-button">Buy Now</Link>
+              <Link to="/shopnfccards/whitecard" className="desktop-button blue-button">Buy Now</Link>
             </div>
           </div>
         </div>
