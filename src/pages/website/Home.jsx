522227d4f6348a2cf19ff8a0525cfa9026bc794a
Jarek <103086uk@saeinstitute.edu>
2025-08-30 05:08:06 +0100

    Automated update

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -97,7 +97,7 @@ export default function Home() {
         <div className="hero-container">
           <div className="hero-left">
             <h1 className="desktop-h1 hero-heading">Stand Out. Get Noticed. Grow Your <span className='blue'>Business.</span></h1>
-            <p className="desktop-h6 desktop-body">
+            <p style={{fontWeight: 600}} className="desktop-h6 desktop-body">
               Build a professional profile that gets you noticed, and share it effortlessly through your Konar Card with a single tap.</p>
 
             <div className="hero-cta">
