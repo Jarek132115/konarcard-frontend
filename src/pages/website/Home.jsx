a06037866ce187a65d929cedab06d2e4366125b1
Jarek <103086uk@saeinstitute.edu>
2025-09-01 05:16:08 +0100

    Automated update

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -400,7 +400,7 @@ export default function Home() {
                 ].map((text, i) => (
                   <li className="pricing-feature" key={i}>
                     <img src={TickIcon} alt="" className="pricing-check invert-for-blue" />
-                    <span className='black desktop-body-x'>{text}</span>
+                    <span style={{fontWeight: 600}} className='white desktop-body-x'>{text}</span>
                   </li>
                 ))}
               </ul>
