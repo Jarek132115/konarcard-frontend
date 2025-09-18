2fc3f74e10ce1bba3f077af5638c538d2ffcbc7d
Jarek <103086uk@saeinstitute.edu>
2025-09-18 04:01:49 +0100

    Automated update

--- src/pages/website/KonarCard.jsx
+++ src/pages/website/KonarCard.jsx
@@ -34,6 +34,7 @@ import PencilIcon from '../../assets/icons/Pencil-Icon.svg';
 import PhoneIcon from '../../assets/icons/Phone-Icon.svg';
 import WalletIcon from '../../assets/icons/Wallet-Icon.svg';
 import QRCode from '../../assets/icons/QR-Code-Icon.svg';
+import NFCChipIcon from '../../assets/icons/NFCChip-Icon.svg';
 
 // NEW: auth + api
 import { AuthContext } from '../../components/AuthContext';
