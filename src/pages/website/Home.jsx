cf1b14e8c9a501d3cb280d55a0327927b1ec22e9
Jarek <103086uk@saeinstitute.edu>
2025-09-18 12:22:13 +0100

    Automated update

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -413,7 +413,7 @@ export default function Home() {
               <div className="pricing-divider" />
               <div className="pricing-price-row">
                 <span style={{ paddingRight: 5 }} className="desktop-h3">£4.95</span>
-                <span style={{ padding: 0 }} className="desktop-button">/Month - Cancel Anytime</span>
+                <span style={{ padding: 0 }} className="desktop-button">/Month - After 14 Days</span>
               </div>
 
               <ul className="pricing-features">
