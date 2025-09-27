8ed6a73d47a3716a47a7a9bc5db17a0cd17ffde7
Jarek <103086uk@saeinstitute.edu>
2025-09-27 03:21:01 +0100

    Automated update

--- src/main.jsx
+++ src/main.jsx
@@ -27,6 +27,8 @@ import './styling/contactinterface.css';
 import './styling/nfccard.css';
 import './styling/myorders.css';
 import './styling/admin.css';
+import './styling/preview.css';
+import './styling/editor.css';
 
 import App from './App.jsx';
 import { AuthProvider } from './components/AuthContext';
