b51b735a76dc4c1f423fea6026e45c97c2e5a561
Jarek <jarek.kon0709@gmail.com>
2026-01-30 16:03:20 +0000

    Update frontend

--- src/pages/website/Blog.jsx
+++ src/pages/website/Blog.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
-import "../../styling/design-system.css";
+import "../../styling/fonts.css";
 import "../../styling/blog.css";
 
 export default function Blog() {
