96e474c1ecd699c741fe4ff8f1dd7ad1dc36ee4b
Jarek <jarek.kon0709@gmail.com>
2026-02-03 19:03:42 +0000

    Update frontend

--- src/components/Dashboard/Editor.jsx
+++ src/components/Dashboard/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 import { previewPlaceholders } from "../../store/businessCardStore";
 
 /* Social icons */
