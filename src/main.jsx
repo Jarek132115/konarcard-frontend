1444ccfe08abcb90caaef906d04cfc476540a456
Jarek <jarek.kon0709@gmail.com>
2026-02-11 17:26:01 +0000

    Update frontend

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createHead, UnheadProvider } from "@unhead/react";
+
+/** ✅ Unhead */
+import { createHead } from "unhead";
+import { UnheadProvider } from "@unhead/react";
 
 import "./styling/fonts.css";
 import "./styling/login.css";
